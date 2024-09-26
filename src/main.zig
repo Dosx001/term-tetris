8ce7578ef80cc8e1c19675606166cc2c9d22b380
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-09-26 16:07:47 -0700

    feat: create title window

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,13 @@ const nc = @cImport(@cInclude("ncurses.h"));
 
 pub fn main() !void {
     _ = nc.initscr();
+    _ = nc.noecho();
+    const title: ?*nc.WINDOW = nc.newwin(10, 30, 2, 65);
     _ = nc.refresh();
+    _ = nc.box(title, 0, 0);
+    _ = nc.wmove(title, 5, 7);
+    _ = nc.waddstr(title, "Terminal Tetris");
+    _ = nc.wrefresh(title);
     _ = nc.getch();
     _ = nc.endwin();
 }
