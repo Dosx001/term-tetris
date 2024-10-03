e31f1b48b5ebeaeaa1d2179e3c83f3943ecf266b
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-10-03 16:39:46 -0700

    perf: inline loops

--- src/game/game.zig
+++ src/game/game.zig
@@ -8,42 +8,34 @@ pub const Game = struct {
     pub fn insert(_: *Game, shape: Shape, state: *[24][10]Color) void {
         switch (shape) {
             .I => {
-                for (3..7) |i| {
-                    state[0][i] = .Cyan;
-                }
+                inline for (3..7) |i| state[0][i] = .Cyan;
             },
             .J => {
                 state[0][3] = .Blue;
-                for (3..6) |i| {
-                    state[1][i] = .Blue;
-                }
+                inline for (3..6) |i| state[1][i] = .Blue;
             },
             .L => {
                 state[0][5] = .Orange;
-                for (3..6) |i| {
-                    state[1][i] = .Orange;
-                }
+                inline for (3..6) |i| state[1][i] = .Orange;
             },
             .O => {
-                for (4..6) |i| {
+                inline for (4..6) |i| {
                     state[0][i] = .Yellow;
                     state[1][i] = .Yellow;
                 }
             },
             .S => {
-                for (3..5) |i| {
+                inline for (3..5) |i| {
                     state[0][i + 1] = .Green;
                     state[1][i] = .Green;
                 }
             },
             .T => {
                 state[0][4] = .Magenta;
-                for (3..6) |i| {
-                    state[1][i] = .Magenta;
-                }
+                inline for (3..6) |i| state[1][i] = .Magenta;
             },
             .Z => {
-                for (3..5) |i| {
+                inline for (3..5) |i| {
                     state[0][i] = .Red;
                     state[1][i + 1] = .Red;
                 }
