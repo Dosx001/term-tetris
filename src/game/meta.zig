c16b0aaac4d34bb15957c9914604c25573625ddc
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-10-07 18:35:17 -0700

    feat: add levels

--- src/game/meta.zig
+++ src/game/meta.zig
@@ -4,9 +4,42 @@ const c = @cImport({
     @cInclude("ncurses.h");
 });
 
+const levels = [29]u16{
+    10,
+    30,
+    60,
+    100,
+    150,
+    210,
+    280,
+    360,
+    450,
+    550,
+    650,
+    750,
+    850,
+    950,
+    1050,
+    1150,
+    1260,
+    1380,
+    1510,
+    1650,
+    1800,
+    1960,
+    2130,
+    2310,
+    2500,
+    2700,
+    2900,
+    3100,
+    3300,
+};
+
 pub const Meta = struct {
     win: ?*c.WINDOW,
     lines: usize = 0,
+    level: usize = 0,
     pub fn init() Meta {
         const win = c.newwin(8, 12, 17, 0);
         _ = c.mvwaddstr(win, 1, 1,
@@ -48,6 +81,10 @@ pub const Meta = struct {
         while (0 <= j) : (j -= 1) {
             std.mem.swap([10]Color, &state[i], &state[@intCast(j)]);
             i -= 1;
+        }
+        if (self.level != 30 and levels[self.level] <= self.lines) {
+            self.level += 1;
+            _ = c.mvwprintw(self.win, 4, 1, "%i", self.level);
         }
         _ = c.mvwprintw(self.win, 6, 1, "%i", self.lines);
         _ = c.wrefresh(self.win);
